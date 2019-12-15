da3900671e1dcdfee9404abb30bcd8116d865143
Jacquelyn Samaniego <samaniego_jackie@yahoo.com>
2019-12-15 05:47:44 +0000

    created validation in the app

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,12 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   end
 
   def create
-    current_user.places.create(place_params)
+     @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
   end
 
   def show
@@ -32,7 +36,11 @@ before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destro
   end 
   
    @place.update_attributes(place_params)
-   redirect_to root_path
+    if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
   end
 
   def destroy
