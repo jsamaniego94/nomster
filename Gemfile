175658ad44130f7f724d0f0cf443369600f89936
Jacquelyn Samaniego <samaniego_jackie@yahoo.com>
2020-01-05 17:45:37 +0000

    added geocoder

--- Gemfile
+++ Gemfile
@@ -69,3 +69,5 @@ source 'https://rails-assets.org' do
 end
 gem "font-awesome-rails"
 gem 'devise'
+gem "geocoder"
+gem "figaro"
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
   validates :name, presence: true
-end
+  validates :name, length: {minimum: 3}, presence: true
+  validates :address, presence: true
+  validates :description, presence: true
+ 
+end 
