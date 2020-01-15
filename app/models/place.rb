9fa5181980bfc11ecca9127a931996df66778b18
Jacquelyn Samaniego <samaniego_jackie@yahoo.com>
2020-01-15 01:44:50 +0000

    Added form for place page

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,8 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
-  mas_many :comments
+  has_many :comments
+
   geocoded_by :address
   after_validation :geocode
+  
   validates :name, presence: true
   validates :name, length: {minimum: 3}, presence: true
   validates :address, presence: true
